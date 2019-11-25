777609aa7abea37cbc3c08b2d74f8e8b882dff0b
Marcus Mouyat <marcus@mouyat.com>
2019-11-25 15:42:08 +1100

    Added a JenkinsBuild script.

---
+++ Assets/Editor/JenkinsBuild.cs
@@ -0,0 +1,97 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Assets/Editor/JenkinsBuild.cs
+// -------------------------------------------------------------------------------------------------
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+
+// ------------------------------------------------------------------------
+// https://docs.unity3d.com/Manual/CommandLineArguments.html
+// ------------------------------------------------------------------------
+public class JenkinsBuild
+{
+
+    static string[] EnabledScenes = FindEnabledEditorScenes();
+    // ------------------------------------------------------------------------
+    // called from Jenkins
+    // ------------------------------------------------------------------------
+    [MenuItem("Build/Windows")]
+    public static void BuildWindows()
+    {
+        string appName = "TestProject-Release";
+        string targetDir = "Builds/Windows";
+        string fullPathAndName = targetDir + System.IO.Path.DirectorySeparatorChar + appName + ".exe";
+        BuildProject(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    [MenuItem("Build/Android")]
+    public static void BuildAndroid()
+    {
+        string appName = "TestProject-Release";
+        string targetDir = "Builds/Android";
+        string fullPathAndName = targetDir + System.IO.Path.DirectorySeparatorChar + appName + ".apk";
+        BuildProject(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.Android, BuildOptions.None);
+    }
+
+    [MenuItem("Build/MacOS")]
+    public static void BuildMacOS()
+    {
+
+        string appName = "TestProject-Release";
+        string targetDir = "Builds/Mac OSX";
+        string fullPathAndName = targetDir + System.IO.Path.DirectorySeparatorChar + appName + ".app";
+        BuildProject(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX, BuildOptions.None);
+    }
+
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    private static string[] FindEnabledEditorScenes()
+    {
+
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                EditorScenes.Add(scene.path);
+            }
+        }
+        return EditorScenes.ToArray();
+    }
+
+    // ------------------------------------------------------------------------
+    // e.g. BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX
+    // ------------------------------------------------------------------------
+    private static void BuildProject(string[] scenes, string targetDir, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
+    {
+        PlayerSettings.keyaliasPass = "Qpalzm.10";
+        PlayerSettings.keystorePass = "Qpalzm.10";
+
+        System.Console.WriteLine("[JenkinsBuild] Building:" + targetDir + " buildTargetGroup:" + buildTargetGroup.ToString() + " buildTarget:" + buildTarget.ToString());
+
+        // https://docs.unity3d.com/ScriptReference/EditorUserBuildSettings.SwitchActiveBuildTarget.html
+        bool switchResult = EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        if (switchResult)
+        {
+            System.Console.WriteLine("[JenkinsBuild] Successfully changed Build Target to: " + buildTarget.ToString());
+        }
+        else
+        {
+            System.Console.WriteLine("[JenkinsBuild] Unable to change Build Target to: " + buildTarget.ToString() + " Exiting...");
+            return;
+        }
+
+        // https://docs.unity3d.com/ScriptReference/BuildPipeline.BuildPlayer.html
+        BuildReport buildReport = BuildPipeline.BuildPlayer(scenes, targetDir, buildTarget, buildOptions);
+        BuildSummary buildSummary = buildReport.summary;
+        if (buildSummary.result == BuildResult.Succeeded)
+        {
+            System.Console.WriteLine("[JenkinsBuild] Build Success: Time:" + buildSummary.totalTime + " Size:" + buildSummary.totalSize + " bytes");
+        }
+        else
+        {
+            System.Console.WriteLine("[JenkinsBuild] Build Failed: Time:" + buildSummary.totalTime + " Total Errors:" + buildSummary.totalErrors);
+        }
+    }
+}
