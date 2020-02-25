9bff3fe9fd1c1a8d82e4b3884713a927bc804902
Marcus Mouyat <marcus@mouyat.com>
2020-02-25 15:30:13 +1100

    Updated the player's HUD, and finalised shot debris

--- Assets/FieldOfViewTest.cs
+++ Assets/FieldOfViewTest.cs
@@ -13,7 +13,7 @@ public class FieldOfViewTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        List<GameObject> o = GetFieldOfView(transform, 100, 60, 30);
+        List<GameObject> o = GetFieldOfView(transform, 100, 60, 60);
         string list = "FOV: ";
         foreach(GameObject obj in o)
         {
@@ -33,15 +33,20 @@ public class FieldOfViewTest : MonoBehaviour
             {
                 if (lineOfSight.collider == c) // If raycast hits object being checked for line of sight.
                 {
+                    /*
                     Vector3 relativePosition_X = new Vector3(c.transform.position.x, viewOrigin.position.y, c.transform.position.z) - viewOrigin.position;
                     Vector3 relativePosition_Y = new Vector3(viewOrigin.position.x, c.transform.position.y, c.transform.position.z) - viewOrigin.position;
+                    */
+
+                    // Obtains the horizontal and vertical relative position data for the raycast hit point relative to the line of sight's origin.
+                    Vector3 relativePosition_X = new Vector3(lineOfSight.point.x, viewOrigin.position.y, lineOfSight.point.z) - viewOrigin.position;
+                    Vector3 relativePosition_Y = new Vector3(viewOrigin.position.x, lineOfSight.point.y, lineOfSight.point.z) - viewOrigin.position;
+
                     Vector2 visionAngle = new Vector2(Vector3.Angle(relativePosition_X, viewOrigin.forward), Vector3.Angle(relativePosition_Y, viewOrigin.forward));
                     if (visionAngle.x < horizontalFOV && visionAngle.y < verticalFOV)
                     {
-                        print("NPC " + gameObject.name + "has spotted " + c.gameObject.name + ".");
+                        objectsInView.Add(c.gameObject); // Add c.gameObject to viewedObjects array, I need to figure out how to do this!
 
-                        // Add c.gameObject to viewedObjects array, I need to figure out how to do this!
-                        objectsInView.Add(c.gameObject);
                     }
                 }
             }
--- Assets/Scripts/Weapons/Projectiles/Bullet.cs
+++ Assets/Scripts/Weapons/Projectiles/Bullet.cs
@@ -9,22 +9,9 @@ public class Bullet : KineticProjectile
     
     public override void OnHit()
     {
-        //GameObject ie = 
-        //Quaternion normalDirection = Quaternion.LookRotation(-projectileHit.normal, Vector3.up);
-
-
-        //Quaternion normalDirection = Quaternion.FromToRotation(Vector3.back, projectileHit.normal);
-
-
-        //Quaternion.FromToRotation(Vector3.back, surfaceFound.normal)
-
-
-        //Instantiate(impactEffect.gameObject, projectileHit.point + normalDirection * transform.forward, normalDirection);
-
         Quaternion normalDirection = Quaternion.FromToRotation(Vector3.forward, projectileHit.normal);
         Instantiate(impactEffect.gameObject, projectileHit.point + normalDirection * Vector3.forward * 0.1f, normalDirection);
-        //impactEffect.GetComponent<ParticleSystemRenderer>().material = projectileHit.collider.GetComponent<Renderer>().material;
+        //Instantiate(impactEffect.gameObject, projectileHit.point + normalDirection * Vector3.forward * 0.001f, normalDirection, projectileHit.collider.transform);
         base.OnHit();
     }
-    
 }
