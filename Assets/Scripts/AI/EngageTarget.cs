6ecef316493316993fd69b680d6dedbf0012d4cd
Cptn. Dynamite Fabulous <marcus@mouyat.com>
2020-03-21 12:04:31 +1100

    Started to complete complex state machine for gunman AI

--- Assets/Scripts/AI/EngageTarget.cs
+++ Assets/Scripts/AI/EngageTarget.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Follow : AIMovementBehaviour
+public class EngageTarget : AIMovementBehaviour
 {
     Transform targetLocation;
     NullableVector3 currentDestination;
