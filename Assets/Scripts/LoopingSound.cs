db42bc3ff97aa264c1052b11927764d994a0b58a
CptnFabulous <marcus@mouyat.com>
2020-12-08 17:20:35 +1100

    Changelog 08/12/2020
    Began some work creating an improved system to play repeating or looping sound effects. One problem with repeating the same effect over and over is that an uncanny-valley-esque effect sets in. In real life, when performing the same noisy action multiple times, there will often be subtle differences in the way an action is performed that results in a slightly different sound. E.g. different footsteps due to stepping on different materials or bodyweight causing the person's foot to hit the ground at a different speed and angle. I intend on trying several methods of accomplishing this:
    
    * Randomly selecting one from several similar sounds
    
    * Having one sound and randomly altering the pitch by a small margin in real time
    
    * Having a single loop of different sounds

---
+++ Assets/Scripts/LoopingSound.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingSound : ScriptableObject
+{
+    public AudioClip start;
+    public AudioClip loop;
+    public AudioClip end;
+    float soundDelay;
+
+
+    public IEnumerator Play(AudioSource source)
+    {
+        source.loop = false;
+        source.clip = start;
+        source.Play();
+
+        yield return new WaitWhile(() => source.time < start.length);
+
+        source.loop = true;
+        source.clip = loop;
+        source.Play();
+    }
+
+    void End(AudioSource source)
+    {
+        source.loop = false;
+        source.clip = end;
+        source.Play();
+    }
+
+    void Cancel(AudioSource source)
+    {
+        source.Stop();
+        source.clip = null;
+    }
+
+    void Restart(AudioSource source)
+    {
+        Cancel(source);
+        Play(source);
+    }
+}
+
+public class RepeatingSound : ScriptableObject
+{
+    public AudioClip[] clips;
+    public float delay;
+
+    // Add specific audio data as well, so I can adjust on the fly
+
+    public void PlaySound(AudioSource source)
+    {
+        // Randomly selects a clip
+        AudioClip randomClip = clips[Random.Range(0, clips.Length - 1)];
+        source.PlayOneShot(randomClip);
+    }
+}
