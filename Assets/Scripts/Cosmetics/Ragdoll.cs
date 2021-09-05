b8eaf7c62d86fa7681394aafdd100b498e67af41
Cptn. Dynamite Fabulous <marcus@mouyat.com>
2021-09-06 00:24:17 +1000

    Update 05/09/2021: Work on ragdoll physics
    With my AI code mostly fixed, I opted to do some more work on making the game prettier. A big problem getting in the way of the game’s appearance is that enemies will immediately disappear on death with no animations, which would be very silly-looking. Since I don’t presently have access to complex death animations, nor the time to rig them up, I opted for a classic, versatile and hilarious solution – ragdoll physics. My plan will be that upon death, the enemy’s ragdoll will activate and cause them to collapse to the ground, in a simple but highly effective manner. While this won’t be perfect for all deaths in the game, it will be immensely useful for a smaller production, especially later down the line if I still decide I can’t afford to get better animations.
    
    I started by taking the placeholder soldier model I am using for my enemy animations (which already has hitbox colliders attached), and saving it as a prefab for easy editing. I then started to, for each component, adding a rigidbody and a Character Joint, to realistically constrain each joint’s movement relative to its parent. I also created a ‘Ragdoll’ MonoBehaviour script that obtains the ragdoll’s various colliders, rigidbodies and character joints as arrays, so it can automate assigning certain variables and enable/disable them. The script also has an easy function for enabling/disabling the script, by setting the kinematic values of the rigidbodies.
    
    The system is unfinished and very buggy, with the main issues being each joint not staying attached by distance to its parent, stretching away easily. However, this system shows promise, and I feel I have the fundamental elements down. I believe the main challenge will be mastering the Character Joint component, to ensure they behave in the ways I want it to.

---
+++ Assets/Scripts/Cosmetics/Ragdoll.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    CharacterJoint[] joints;
+    Collider[] colliders;
+    Rigidbody[] rigidbodies;
+    public bool startActive;
+    public bool disableCollidersWhenDisabled;
+
+
+
+    private void Awake()
+    {
+        joints = GetComponentsInChildren<CharacterJoint>();
+        colliders = GetComponentsInChildren<Collider>();
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+        
+        for (int i = 0; i < joints.Length; i++)
+        {
+            joints[i].enableCollision = false;
+            joints[i].connectedBody = joints[i].transform.parent.GetComponentInParent<Rigidbody>();
+            Debug.Log("Setting rigidbody of " + joints[i].name + " to " + joints[i].connectedBody);
+        }
+        SetActive(enabled);
+    }
+
+    void SetActive(bool active)
+    {
+        /*
+        for (int i = 0; i < joints.Length; i++)
+        {
+            //joints[i].en
+        }
+        */
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            // If the ragdoll is activated, or if it's deactivated but disableCollidersWhenDisabled is false, activate the collider.
+            // If the ragdoll is deactivated and disableCollidersWhenDisabled is true, disable it
+            colliders[i].enabled = !(!active && disableCollidersWhenDisabled);
+        }
+        for (int i = 0; i < rigidbodies.Length; i++)
+        {
+            rigidbodies[i].isKinematic = !active;
+        }
+    }
+
+    private void OnEnable()
+    {
+        SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Events/EventJunction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventJunction
+{
+    public static event System.Action<AttackMessage> OnAttack;
+    public static event System.Action<DamageMessage> OnDamage;
+    public static event System.Action<KillMessage> OnKill;
+    public static event System.Action<InteractMessage> OnInteract;
+    public static event System.Action<SpawnMessage> OnSpawn;
+
+
+    public static void TransmitAttack(AttackMessage message)
+    {
+        OnAttack(message);
+    }
+    public static void TransmitDamage(DamageMessage message)
+    {
+        OnDamage(message);
+    }
+    public static void TransmitKill(KillMessage message)
+    {
+        OnKill(message);
+    }
+    public static void TransmitInteraction(InteractMessage message)
+    {
+        OnInteract(message);
+    }
+    public static void TransmitSpawning(SpawnMessage message)
+    {
+        OnSpawn(message);
+    }
+
+    public static void UpdateAttackReceiver(System.Action<AttackMessage> function, bool isAdding)
+    {
+        if (isAdding)
+        {
+            OnAttack += function;
+        }
+        else
+        {
+            OnAttack -= function;
+        }
+    }
+}
